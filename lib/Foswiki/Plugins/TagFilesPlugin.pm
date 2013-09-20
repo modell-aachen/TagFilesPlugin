df50bd5b67c1e1d159c6cf6f52dd30a862a9c27e
Stephan Osthold <osthold@modell-aachen.de>
2013-09-20 11:01:15 +0200

    Release 0.0.4

--- lib/Foswiki/Plugins/TagFilesPlugin.pm
+++ lib/Foswiki/Plugins/TagFilesPlugin.pm
@@ -12,7 +12,7 @@ use Error ':try';
 use URI::Escape;
 
 our $VERSION          = '$Rev: 7808 (2010-06-15) $';
-our $RELEASE          = '0.0.3';
+our $RELEASE          = '0.0.4';
 
 our $SHORTDESCRIPTION = 'Tag files in your wiki.';
 
